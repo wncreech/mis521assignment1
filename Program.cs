31d6f7d8413cfbde44d439e1404bb9e0236e1d7e
William Creech <wncreech@crimson.ua.edu>
2025-09-14 21:43:21 -0500

    initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace FL2025_Assignment1_wcreech
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
